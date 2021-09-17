7eeaa475e3d52cd8cfbefaae0531d6a1f6183e11
Mira <prorammerMira@gmail.com>
2021-09-17 22:56:12 +0300

    add schedule (every minute update)

--- telegramBotTest/updater.py
+++ telegramBotTest/updater.py
@@ -1,5 +1,8 @@
 #updates endpoints every 15 minutes
 #region imports
+import schedule
+import time
+
 from Connections.DataBaseConnection import DataBaseConnection
 from Services.BotServices.JsonParser import JsonParser
 from Services.BotServices.ConfigReader import ConfigReader
@@ -31,6 +34,7 @@ databaseDeleter = DataBaseDeleter(databaseConnection)
 
 #region debug area: test con. to db
 #input must be chat id, e.g. "-536304400"
+"""
 chatID = "-536304400"
 databaseWriter.WriteChat(chatID)
 databaseWriter.WriteEndpointAndChat([chatID, "www.vk.com", "vk main page", endpointValidityCheck.Check("www.vk.com")])
@@ -51,9 +55,21 @@ print(res)
 
 other_res = databaseReader.ReadEndpointsForChat("-536304400")
 print(other_res)
-
+"""
 #endregion
 
 #region logic
+def update():
+    endpoints = databaseReader.ReadAllEndpoints()
+    for endpoint in endpoints:
+        validity = endpointValidityCheck.Check(endpoint[0])
+        databaseUpdater.UpdateEndpointStateAndDescription([endpoint[0],validity])
+        print("Updated: endpoint: {} with state: {}".format(endpoint[0],validity))
+
+schedule.every(1).minutes.do(update)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 #endregion
