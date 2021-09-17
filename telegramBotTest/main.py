796a7dca26a06e9b972f2807c52ad8da01c94125
Mira <prorammerMira@gmail.com>
2021-09-18 01:39:38 +0300

    Update main.py

--- telegramBotTest/main.py
+++ telegramBotTest/main.py
@@ -49,7 +49,13 @@ def welcome(message):
 
 @bot.message_handler(commands=['add'])
 def addition(message):
-    print(message.text)
+    endpoint_data = str(message.text).replace('/add','')
+    endpoint_name = endpoint_data[0:endpoint_data.find(' ')]
+    endpoint_description = endpoint_data[endpoint_data.find(' '):-1]
+    endpoint_state = endpointValidityCheck.Check(endpoint_name)
+    print(endpoint_name)
+    print(endpoint_description)
+    print(endpoint_state)
     welcome(message)
 
 #inline menu relies
