cc771bceecf55b5a4ca0e6e2bd5e3d953104f660
Mira <prorammerMira@gmail.com>
2021-09-17 22:55:32 +0300

    fix update: remove description update

--- telegramBotTest/Services/Database/DataBaseUpdater.py
+++ telegramBotTest/Services/Database/DataBaseUpdater.py
@@ -6,17 +6,17 @@ class DataBaseUpdater(object):
     def __init__(self, connection):
         self.__connection = connection
 
-    def UpdateEndpointStateAndDescription(self, data):
+    def UpdateEndpointState(self, data):
         """
             Updates info about endpoint
-            data => [chat_id, endpoint_name, endpoint_description, endpoint_state]
+            data => [endpoint_name, endpoint_state]
         """
         try:
             with self.__connection.Connection.cursor() as cursor:
-                cursor.execute(self.__queryUpdateEndpoint.format(str(data[2]),str(data[3]),str(datetime.now()),str(data[1])))
+                cursor.execute(self.__queryUpdateEndpoint.format(str(data[1]),str(datetime.now()),str(data[0])))
         except Exception as e:
-            print("DataBaseUpdater Exception UpdateEndpointStateAndDescription: ",e)
+            print("DataBaseUpdater Exception UpdateEndpointState: ",e)
 
     __connection = None
 
-    __queryUpdateEndpoint = "UPDATE endpoints SET description='{}', state={}, updated_at='{}' WHERE name='{}';"
+    __queryUpdateEndpoint = "UPDATE endpoints SET state={}, updated_at='{}' WHERE name='{}';"
